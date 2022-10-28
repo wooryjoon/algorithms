26911b898ad5fa9f50e32223850657927c588ccc
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2022-10-28 16:04:21 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+
+A = int(input())
+
+if A >= 90 and A <= 100 :
+    print('A')
+elif A >= 80 :
+    print('B')
+elif A >= 70 :
+    print('C')
+elif A >= 60 :
+    print('D')
+else:
+    print('F')
