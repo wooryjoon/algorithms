fee9d74e1204e2b3811b12212854a4581f696761
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2022-11-16 11:31:31 +0900

    [Silver V] Title: 거스름돈, Time: 152 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.js
+++ 백준/Silver/14916. 거스름돈/거스름돈.js
@@ -3,25 +3,19 @@ const input = fs.readFileSync("/dev/stdin").toString().trim();
 
 const solution = (N) => {
   let answer = 0;
-  // 1, 3 예외처리 따로 해주기, 나머지는 그대로.
+  // if 5로 최대한 나눴을 때 나머지가 홀수이면 nFive를 -1해준다
+  // else nFive 그대로 유지.
+  // N = N- nFive 로 바꾸고 N을 2로 나눠준다.
+  // 예외사항 : 1,3 일때는 거스름돈을 줄 수가 없으므로 -1을 반환하게 한다.
   if (N == "1" || N == "3") return -1;
-  if (N == "6") return 3;
-  if (N == "8") return 4;
-  else if (
-    N[N.length - 1] == "1" ||
-    N[N.length - 1] == "3" ||
-    N[N.length - 1] == "6" ||
-    N[N.length - 1] == "8"
-  ) {
-    answer += Math.floor(N / 5) - 1;
-    N = N - answer * 5;
-    answer += Math.floor(N / 2);
-  } else {
-    answer += Math.floor(N / 5);
-    N = N % 5;
-    answer += Math.floor(N / 2);
-  }
-  return answer;
+
+  let nFive = parseInt(N / 5);
+  let rest = N % 5;
+
+  if (rest % 2 == 1) nFive--;
+  N = N - 5 * nFive;
+
+  return nFive + parseInt(N / 2);
 };
 
 console.log(solution(input));
