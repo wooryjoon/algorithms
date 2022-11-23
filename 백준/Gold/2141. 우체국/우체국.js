057e1e75009040f2c63e8d39f50200e427fe8e0b
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2022-11-23 12:50:40 +0900

    [Gold IV] Title: 우체국, Time: 416 ms, Memory: 47296 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.js
+++ 백준/Gold/2141. 우체국/우체국.js
@@ -10,7 +10,9 @@ function sol(n, cityAndPeople) {
   let people = 0;
   let total = 0;
   //마을의 위치를 기준으로 오름차순 정렬 후, 전체 인구의 절반을 넘는 그 순간의 마을 번호를 출력.
-  cityAndPeople.sort((a, b) => a[0] - b[0]); // 오름차순 정렬
+  cityAndPeople.sort((a, b) => {
+    return a[0] - b[0];
+  }); // 오름차순 정렬
   total = cityAndPeople.reduce((acc, cur) => acc + cur[1], 0);
   let half = total / 2;
   for (let i = 0; i < cityAndPeople.length; i++) {
