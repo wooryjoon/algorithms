3bb0d7dd6aed187be8482a77511002c9c7090bb2
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2022-11-27 18:12:24 +0900

    [level 2] Title: 소수 찾기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,30 +1,33 @@
-function solution(numbers) {
-  let prime_set = new Set();
-  let arr = numbers.split("");
+function solution (numbers) {
+    let arr = numbers.split('');
+    let visited = new Array(arr.length+1).fill(false); // arr의 각 항을 방문처리할 visited 배열
+    let answer  = new Set(); // answer Set에 답이되는걸 저장할거임, 근데 중복을 제외할거니까 Set객체로
+    // 순열 (순서가 보장되야함)
     
-  function isPrime(n) {
-    if (n <= 1) return false;
-    for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
-      if (n % i === 0) return false;
+        const isPrime = (number) => {
+        if (number <= 1) return false; // 1이하면 소수아님
+        if (number % 2 == 0 && number != 2) return false; // 2가아니면서 2로 나누어떨어지면 소수아님
+        for (let i = 3; i <= Math.floor(Math.sqrt(number)); i += 2){
+            if (number % i == 0) return false;
+        }
+        return true; // 위에서 다 안걸러졌으면 그건 소수임.
+}
+    const DFS = (num) => {
+        if (num) { // 숫자일 때
+            if (isPrime(parseInt(num)) == true) answer.add(parseInt(num));
+        }
+        
+        //수행연산
+        for (let i = 0; i < arr.length; i++){
+            let add_num = arr[i];
+            if (visited[i] == true) continue; // 만약에 쓴 숫자면 패스
+            visited[i] = true;
+            DFS(num + add_num);
+            visited[i] = false; // 순서가 보장되어야함.
+        }
+        
     }
-    return true;
-  }
-  function DFS(combination, others) {
-    if (combination != "") { 
-      if (isPrime(parseInt(combination))) {
-        prime_set.add(parseInt(combination));
-      }
-    }
-
-    for (let i = 0; i < others.length; i++) {
-      let news = others.slice();
-      news.splice(i, 1);
-      DFS(combination + others[i], news); // numbers의 각 문자가 첫글자가 되는 case들마다 DFS 실행, 
-    }
-  }
-
-  //모든 조합을 만드는 recursive
-  DFS("", arr); // 인자 : 현재까지 조합된 수, 조합된 수를 제외한 나머지 후보
-
-  return prime_set.size;
+    
+DFS('');
+return answer.size;
 }
