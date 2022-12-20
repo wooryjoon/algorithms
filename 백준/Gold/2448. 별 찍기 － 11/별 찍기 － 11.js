b9f7fba623f0180e8bc5a57ccec4465dc8d61c09
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2022-12-20 16:40:38 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 1272 ms, Memory: 197332 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.js
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.js
@@ -6,23 +6,24 @@ let map = Array.from(Array(N), () => Array(2 * N - 1).fill(" "));
 const printStar = (N, i, j) => {
   // 그려야할 지도 사이즈, 기준 좌표 i,j
   if (N == 3) {
-    map[i][j + 2] = "*";
+    map[i][j] = "*";
+    map[i + 1][j - 1] = "*";
     map[i + 1][j + 1] = "*";
-    map[i + 1][j + 3] = "*";
-    for (let k = 0; k <= 4; k++) {
+    for (let k = -2; k <= 2; k++) {
       map[i + 2][j + k] = "*";
     }
     return;
   } else {
     // 좌표에 넣는 x,y는 각 기준점에서의 0,0이되는 값
-    printStar(parseInt(N / 2), i, j + parseInt(N / 2));
-    printStar(parseInt(N / 2), i + parseInt(N / 2), j);
-    printStar(parseInt(N / 2), i + parseInt(N / 2), j + N);
+    printStar(parseInt(N / 2), i, j);
+    printStar(parseInt(N / 2), i + parseInt(N / 2), j - parseInt(N / 2));
+    printStar(parseInt(N / 2), i + parseInt(N / 2), j + parseInt(N / 2));
   }
 };
 
-printStar(Number(N), 0, 0);
+printStar(Number(N), 0, N - 1);
 for (value of map) {
   console.log(value.join(""));
 }
-
+// 이차원 배열 만들고, 각각 섹터를 분할한 후에, 기준점이 되는 좌표 찾기.
+// 맵이 커지면서 ,기준점을 잡는 규칙성 자체를 찾는게 힘들었따. (능지 이슈)
