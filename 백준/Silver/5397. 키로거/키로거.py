b2bcbdb01dad88f0fd1fca8a42619ed44d6d29a1
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2023-01-23 00:17:40 +0900

    [Silver II] Title: 키로거, Time: 892 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -1,21 +1,22 @@
-test_cases = int(input())
+import sys
+input = sys.stdin.readline
 
-for _ in range(test_cases):
-    left = []
+T = int(input())
+# 백스페이스 화살표 주의 , 스택으로 못푸나?
+for _ in range(T): # 테케 두번 반복
+    pwd = input().strip()
+    left  = []
     right = []
-    pwd = input()
-
-    for x in pwd:
-        if x == ">":
-            if right:
-                left.append(right.pop()) 
-        elif x=="<":
-            if left:
+    for x in pwd :
+        if x =='<':
+            if left :
                 right.append(left.pop())
-        elif x=="-":
+        elif x =='>':
+            if right :
+                left.append(right.pop())
+        elif x =='-':
             if left:
                 left.pop()
         else:
             left.append(x)
-
-    print("".join(left)+"".join(reversed(right)))
+    print(''.join(left) + ''.join(reversed(right)))
