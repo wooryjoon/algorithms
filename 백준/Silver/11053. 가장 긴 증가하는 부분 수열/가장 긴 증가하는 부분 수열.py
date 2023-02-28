c22293f0df4a448f8d892e80be4fb43d8ce507ba
wooryjoon <106508216+wooryjoon@users.noreply.github.com>
2023-02-28 18:32:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 180 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,13 +1,28 @@
 import sys
-import math
 input = sys.stdin.readline
-A = int(input())
+from collections import deque
+
+# 문제 정리
+    # LIS 알고리즘
+# 아이디어
+
+#시간복잡도
+
+n = int(input())
+
+d = [1] * n
 arr = list(map(int,input().split()))
-dp = [1 for i in range(A)]
 
-for i in range(A) :
-    for j in range(i) :
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i],dp[j] + 1)
+#d[i] = i번쨰 항까지 LIS의 최대값
 
-print(max(dp))
+for i in range(1,n):
+    for j in range(0,i):
+        if arr[i] > arr[j] :
+            d[i] = max(d[i],d[j]+1)
+print(max(d))
+
+
+
+
+
+
