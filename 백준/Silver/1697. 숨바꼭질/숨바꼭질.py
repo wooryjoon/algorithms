2c0247214c91d7bee2f2de4308977d3a319fc976
heejoon park <wooryjoon@gmail.com>
2023-03-29 21:45:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 208 ms, Memory: 57088 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,32 +1,26 @@
 import sys
+import copy
 input = sys.stdin.readline
 from collections import deque
 
-# 문제 정리
-    # 걷기 : 한칸뒤로 or 한칸 앞으로
-    # 순간이동 : 2배 거리 이동
-    # 수빈이는 동생을 찾고싶다. 가장 빠른 시간내에 찾는 방법구하기 (시간)
-    
-# 아이디어
-    # BFS완탐으로 매 초에 갈 수 있는 모든 case 파악해보자?
-# 변수
-    # ans
+# BFS로 시간 카운트하면서 출발
+# K에 도달했을 때 그때의 시간 리턴.
 
-N,M = list(map(int,input().split()))
-visited = {}
-visited[N] = 1
-MAX = 100001
-def BFS (start) :
-    q = deque()
-    q.append([start,0]) # 현재위치,초
-    while q:
-        curr,sec = q.popleft()
-        if curr == M:
-            return sec
-        for i in [curr,1,-1] :
-            nextCurr = curr + i # 한칸빼기, 한칸더하기, 두배로가기
-            if nextCurr >= 100001 : continue
-            if visited.get(nextCurr) : continue
-            visited[nextCurr] = 1
-            q.append([nextCurr,sec+1])
-print(BFS(N))
+def sol () :
+    n,k = list(map(int,input().split()))
+    visited = {}
+    def BFS():
+        q = deque()
+        q.append((n,0)) # 현재위치, 현재까지 소요시간
+        visited[n] = 1
+        while q :
+            curr,time = q.popleft()
+            if curr == k :
+                return time
+            for e in [curr+1,curr-1,curr+curr]:
+                if e > 100000: continue
+                if visited.get(e) : continue
+                visited[e] = 1
+                q.append((e,time+1))
+    print(BFS())
+sol()
