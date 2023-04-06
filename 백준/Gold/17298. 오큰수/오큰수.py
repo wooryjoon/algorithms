34c9e3ee861af945777855f665518d0eddce5715
heejoon park <wooryjoon@gmail.com>
2023-04-06 23:42:27 +0900

    [Gold IV] Title: 오큰수, Time: 1032 ms, Memory: 263916 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,28 @@
+import sys
+import math
+import copy
+input = sys.stdin.readline
+from collections import deque 
+from itertools import combinations
+import heapq
+
+# 반복문으로 넣고, while문으로 빼기
+
+n = int(input())
+arr = list(map(int,input().split()))
+stack = []
+ans = [0]* n
+for i in range(n) :
+    x = arr[i]
+    if not stack : 
+        stack.append((x,i)) # 값과 위치를 기록  
+        continue
+    while stack :
+        if stack[-1][0] < x :
+            ans[stack.pop()[1]] = x
+        else :break
+    stack.append((x,i))
+
+for e in stack:
+    ans[e[1]] = -1
+print(' '.join(map(str,ans)))
