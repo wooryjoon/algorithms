b21543304f0c491607f9ef29c22cbdd17cef076e
heejoon park <wooryjoon@gmail.com>
2023-04-13 19:10:05 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -2,9 +2,8 @@ def solution(d, budget):
     d.sort()
     ans = 0
     for i in range(len(d)):
-        x = d[i]
-        if budget >= x :
-            budget -= x
+        if budget >= d[i] :
+            budget -= d[i]
             ans += 1
         else : break
     return ans
