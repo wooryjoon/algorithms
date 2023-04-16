1a87afdd3b63c5e9507cef80b617a7bae50082cb
heejoon park <wooryjoon@gmail.com>
2023-04-16 15:07:11 +0900

    [Silver III] Title: 01타일, Time: 332 ms, Memory: 69788 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,24 +1,14 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-# 문제 정리
-    # dp
-# 아이디어
-    # d[1] = 1
-    # d[2] = 2          00 11
-    # d[3] = 3          001 100 111
-    # d[4] = 5          0011 11001 1111 (d3)    
-    # d[5] = 
-#시간복잡도
 
 n = int(input())
-d = [0] * 1000010
-
-d[1] = 1
-d[2] = 2
-d[3] = 3
-
-for i in range(4,n+1) :
-    d[i] = d[i-1]%15746 + d[i-2]%15746
-print(d[n]%15746)
+d = [0] * (n+1)
+if n == 1:
+    print(1)
+else :
+    d[1] = 1
+    d[2] = 2
+    for i in range(3,n+1):
+        d[i] = (d[i-1]+d[i-2]) % 15746
+    print(d[n])
