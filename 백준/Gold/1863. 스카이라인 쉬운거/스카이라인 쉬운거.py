896aba454ef59ad8355105a2c98d5b2e69af1265
heejoon park <wooryjoon@gmail.com>
2023-04-20 09:25:00 +0900

    [Gold V] Title: 스카이라인 쉬운거, Time: 56 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -2,10 +2,8 @@ import sys
 input = sys.stdin.readline
 
 # 아이디어
-    # location 반복문  돌면서
-    # prev < curr => 새 빌딩이 있는 것.
-    # prev >= curr 
-        # 
+    # case 분류
+    # 스택을 활용한 효율적 계산
 # 변수 사용
 
 n = int(input())
@@ -15,15 +13,15 @@ stack = [location[0][1]]
 if stack[-1] > 0 : ans += 1
 for i in range(1,n):
     x,y = location[i]
-    if y == stack[-1] : continue # prev와 높이가 같다면 같은빌딩
-    elif y > stack[-1] : # prev보다 높이가 크다면 새 빌딩
+    if y > stack[-1] : # prev보다 높이가 크다면 새 빌딩
         ans += 1
         stack.append(y)
-    elif y < stack[-1] : # prev보다 높이가 작다면, 이전 건물 높이 확인
-        while True :
+    elif y < stack[-1] : # prev보다 높이가 작다면, 이전 건물 높이 확인 By 스택
+        while True : # 이전 건물 중에 현재 건물보다 높이가 작/같 나오면 break 
             stack.pop()
             if not stack or stack[-1] <= y : break
         if not stack or stack[-1] < y : 
             if y :ans += 1
             stack.append(y)
 print(ans)
+
