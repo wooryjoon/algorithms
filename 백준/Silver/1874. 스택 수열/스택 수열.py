0e3bf55a81a537e518e6779142cecf2df7a87e99
heejoon park <wooryjoon@gmail.com>
2023-04-24 10:34:34 +0900

    [Silver II] Title: 스택 수열, Time: 124 ms, Memory: 37480 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,37 +1,34 @@
 import sys
-
 input = sys.stdin.readline
-from collections import deque
 
-# 문제 정리
-    # 스택에 푸쉬하는 순서는 무조건 오름차순 순서대로 간다.   
-# 아이디어
+# 기본 : 해당 수까지 push하고 pop
+# input이 ans[-1]보다 작다면
+# 	pop인데, stack[-1] != input이면
+# 	fail
 
-# 변수 사용 계획
+# input이 ans[-1]보다 크다면 
+# 	해당 수까지 push하고 pop
 
-# 시간 복잡도
 
 n = int(input())
-ans = []
 arr = [int(input()) for _ in range(n)]
-currNum = 1 # push할때 들어갈 수
-currTop = 0
-stack = deque()
-stack.append(0)
+stack = []
+ans = []
+num = 1
 flag = True
-for currInput in arr :
-    while currNum <= currInput:
-        stack.append(currNum)
-        ans.append('+')
-        currNum += 1;
-    if stack[-1] > currInput : 
-        print('NO')
-        flag = False
-        break
-    else : 
+for e in arr :
+    if not ans or e >= num :
+        for x in range(num,e+1):
+            stack.append(x)
+            num += 1
+            ans.append('+')
         stack.pop()
         ans.append('-')
-    
-
-if flag :
-    print('\n'.join(ans))
+    elif e < num :
+        if stack[-1] != e:
+            flag = False
+            break  # 스택수열을 만들 수 없음
+        stack.pop()
+        ans.append('-')
+if flag :print('\n'.join(ans))
+else :print('NO')
