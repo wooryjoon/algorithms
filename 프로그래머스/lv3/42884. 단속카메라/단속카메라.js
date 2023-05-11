3b312a54974e0752e5c1d36dcc4d6303084eb59e
heejoon park <wooryjoon@gmail.com>
2023-05-11 20:37:04 +0900

    [level 3] Title: 단속카메라, Time: 16.18 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
@@ -1,13 +1,28 @@
 function solution(routes) {
-    let camera = [];
-    routes.sort((a,b) => {
-        return a[1]-b[1];
-    }); // 루트 배열을 출구점 기준으로 오름차순 정렬
-    camera.push(routes[0][1]);
-    for (let i = 1; i < routes.length; i++){
-        let [from,to] = routes[i];
-        if (from <= camera[camera.length-1]) continue;
-        camera.push(to);
+    const route = routes.sort((a,b)=>{
+        return b[0]-a[0]
+    })
+    // stack을 돌면서 endPoint는 둘중에 작은 쪽으로 유지됨.
+    ans = 1
+    stack = []
+    endPoint = -1000
+    while (route.length) {
+        if (!stack.length) {
+            stack.push(route.pop())
+            endPoint = stack.at(-1)[1]
+            continue
+        }
+        
+        [nextStart,nextEnd] = route.at(-1)
+        
+        if (nextStart <= endPoint) {
+            route.pop()
+            endPoint = Math.min(endPoint,nextEnd)
+            continue
+        }
+        ans ++
+        stack.push(route.pop())
+        endPoint = stack.at(-1)[1]
     }
-    return camera.length;
+    return ans
 }
