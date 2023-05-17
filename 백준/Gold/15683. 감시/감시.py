303d3c4643bb713733ebfe6966383524f1f702ba
heejoon park <wooryjoon@gmail.com>
2023-05-17 11:47:29 +0900

    [Gold IV] Title: 감시, Time: 1488 ms, Memory: 152396 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,279 +1,139 @@
-import sys
-import copy
-from collections import deque
-input = sys.stdin.readline
-
-#[문제 정리]
-# 1~5 : cctv번호 6 : 벽
-# 벽은 통과 불가능
-# CCTV는 통과 가능
-#CCTV의 비추는 각도의 Case를 DFS완전탐색을 통해 구한다.
-# 그 중에 사각지대의 최소 크기를 출력한다.
-# depth가 CCTV의 개수와 동일해질때가 종료조건
-# [아이디어]
-# 각 CCTV마다 함수를 따로 구현해서, 2차원배열에서 연산하는 함수를 만들고
-# 각 CCTV마다 4방향으로 돌린다.
-# 서로다른위치인데 DFS로 어떻게 연결 ? : 먼저좌표를따고
-# [시간복잡도]
-
-# [변수 사용계획]
-# 갱신해줄 min
-
-# 순서 : 동 남 서 북
-n,m = list(map(int,input().split()))
-board = [list(map(int,input().split())) for _ in range(n)]
-cctvLength = 0
-cctvLocation = []
-mins = 1000
-for i in range(n) : # cctv개수 구하기
-    for j in range(m) :
-        if 1<=board[i][j]<=5:
-            cctvLength += 1
-            cctvLocation.append([i,j])
-cctvLocation.append('Dummy')
-def CCTV1 (newMap,x,y,i) :
-    if i == 0: 
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-    if i == 1 :
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-    if i == 2 : 
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-    if i == 3 : 
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-def CCTV2 (newMap,x,y,i) :
-    if i == 0 : # 동서
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-    if i == 1 : #남북
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-def CCTV3 (newMap,x,y,i) :
-    if i == 0:
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-    if i == 1:
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-    if i == 2:
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-    if i == 3:
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-def CCTV4 (newMap,x,y,i) : 
-    if i == 0:
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-    if i == 1:
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-    if i == 2:
-        # 동
-        for j in range(y,m):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j]== 0 :
-                newMap[x][j] = '#'
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-    if i == 3:
-        # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-        # 서
-        for j in range(y,-1,-1):
-            if newMap[x][j] == 6:
-                break
-            if newMap[x][j] == 0 :
-                newMap[x][j] = '#'
-        #북
-        for j in range(x,-1,-1):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-def CCTV5 (newMap,x,y) : 
-    #북
-    for j in range(x,-1,-1):
-        if newMap[j][y] == 6 :
-            break
-        if newMap[j][y] == 0 :
-            newMap[j][y] = '#'
-    # 동
-    for j in range(y,m):
-        if newMap[x][j] == 6:
-            break
-        if newMap[x][j]== 0 :
-            newMap[x][j] = '#'
-    # 서
-    for j in range(y,-1,-1):
-        if newMap[x][j] == 6:
-            break
-        if newMap[x][j] == 0 :
-            newMap[x][j] = '#'
-    # 남
-        for j in range(x,n):
-            if newMap[j][y] == 6 :
-                break
-            if newMap[j][y] == 0 :
-                newMap[j][y] = '#'
-
-def DFS (currLocation,depth,maps) : 
-    global mins
-    if depth == cctvLength :
-        count = 0
-        for x in maps:
-            for y in x :
-                if y == 0 :
-                    count += 1
-        mins = min(mins,count)
+# CCTV는 벽 통과 못함
+# CCTV는 CCTV를 통과할 수 있다.
+# 6 : 벽, 0 : 빈칸 1~5 : CCTV
+# 각 CCTV가 비출 수 있는 모든 경우 완전탐색
+from copy import deepcopy
+def top(board,x,y):
+    for nx in range(x - 1, -1, -1):
+        if board[nx][y] == 0:
+            board[nx][y] = '#'
+        elif board[nx][y] == 6:
+            return
+    return
+def right(board,x,y) :
+    for ny in range(y+1,M):
+        if board[x][ny] == 0 :
+            board[x][ny] = '#'
+        elif board[x][ny] == 6 :
+            return
+    return
+def bottom(board,x,y) :
+    for nx in range(x+1,N):
+        if board[nx][y] == 0 :
+            board[nx][y] = '#'
+        elif board[nx][y] == 6 :
+            return
+    return
+def left(board,x,y) :
+    for ny in range(y-1,-1,-1):
+        if board[x][ny] == 0 :
+            board[x][ny] = '#'
+        elif board[x][ny] == 6 :
+            return
+    return
+def CCTV1 (board,x,y,direction) :
+    # 상 우 하 좌
+    if direction == 0 : #상
+        top(board,x,y)
+    if direction == 1 : #우
+        right(board,x,y)
+    if direction == 2 : #하
+        bottom(board,x,y)
+    if direction == 3 : #좌
+        left(board,x,y)
+def CCTV2 (board,x,y,direction) :
+    if direction == 0 : #상 하
+        top(board,x,y)
+        bottom(board,x,y)
+    if direction == 1 : #좌 우
+        right(board,x,y)
+        left(board,x,y)
+def CCTV3 (board,x,y,direction) :
+    if direction == 0 : #상 우
+        top(board,x,y)
+        right(board,x,y)
+    if direction == 1 : #우 하
+        right(board,x,y)
+        bottom(board,x,y)
+    if direction == 2 : #하 좌
+        bottom(board,x,y)
+        left(board,x,y)
+    if direction == 3 : #좌 상
+        left(board,x,y)
+        top(board,x,y)
+def CCTV4 (board,x,y,direction) :
+    if direction == 0 : #좌 상 우
+        left(board,x,y)
+        top(board,x,y)
+        right(board,x,y)
+    if direction == 1 : #상 우 하
+        top(board,x,y)
+        right(board,x,y)
+        bottom(board,x,y)
+    if direction == 2 : #우 하 좌
+        right(board,x,y)
+        bottom(board,x,y)
+        left(board,x,y)
+    if direction == 3 : #하 좌 상
+        bottom(board,x,y)
+        left(board,x,y)
+        top(board,x,y)
+def CCTV5 (board,x,y):
+    bottom(board, x, y)
+    left(board, x, y)
+    right(board,x,y)
+    top(board, x, y)
+def check(board) :
+    cnt = 0
+    for arr in board :
+        for e in arr :
+            if e == 0 :
+                cnt += 1
+    return cnt
+def DFS (board,depth) :
+    global result
+    if depth == len(CCTV_location) :
+        result = min(result,check(board))
         return
-    [x,y] = currLocation
-    if board[x][y] == 1: # 4번
+    x,y = CCTV_location[depth]
+    CCTV_Type = board[x][y]
+    if CCTV_Type == 1 :
         for i in range(4):
-            newMap = copy.deepcopy(maps)
-            CCTV1(newMap,x,y,i)
-            DFS(cctvLocation[depth+1],depth+1,newMap)
-    elif board[x][y] == 2: # 2번
+            copy = deepcopy(board)
+            CCTV1(copy,x,y,i)
+            DFS(copy,depth+1)
+    elif CCTV_Type == 2 :
         for i in range(2):
-            newMap = copy.deepcopy(maps)
-            CCTV2(newMap,x,y,i)
-            DFS(cctvLocation[depth+1],depth+1,newMap)
-    elif board[x][y] == 3:
+            copy = deepcopy(board)
+            CCTV2(copy, x, y, i)
+            DFS(copy, depth + 1)
+    elif CCTV_Type == 3 :
         for i in range(4):
-            newMap = copy.deepcopy(maps)
-            CCTV3(newMap,x,y,i)
-            DFS(cctvLocation[depth+1],depth+1,newMap)
-    elif board[x][y] == 4:
+            copy = deepcopy(board)
+            CCTV3(copy,x,y,i)
+            DFS(copy,depth+1)
+    elif CCTV_Type == 4:
         for i in range(4):
-            newMap = copy.deepcopy(maps)
-            CCTV4(newMap,x,y,i)
-            DFS(cctvLocation[depth+1],depth+1,newMap)
-    elif board[x][y] == 5:
-        newMap = copy.deepcopy(maps)
-        CCTV5(newMap,x,y)
-        DFS(cctvLocation[depth+1],depth+1,newMap)
+            copy = deepcopy(board)
+            CCTV4(copy, x, y, i)
+            DFS(copy, depth + 1)
+    elif CCTV_Type == 5:
+        copy = deepcopy(board)
+        CCTV5(copy,x,y)
+        DFS(copy,depth+1)
 
-DFS(cctvLocation[0],0,board)
-print(mins)
+
+N,M = list(map(int,input().split()))
+board = [list(map(int,input().split())) for _ in range(N)]
+CCTV_location = []
+result = 10000
+
+# CCTV가 위치한 좌표 저장
+for i in range(N):
+    for j in range(M):
+        if 1 <= board[i][j] <= 5 :
+            CCTV_location.append((i,j))
+
+
+DFS(board,0)
+print(result)
