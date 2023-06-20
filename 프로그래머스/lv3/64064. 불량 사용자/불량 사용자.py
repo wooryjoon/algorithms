7d2dab7d9cb3943f53c172f0b503f7da564b5387
heejoon park <wooryjoon@gmail.com>
2023-06-20 10:46:42 +0900

    [level 3] Title: 불량 사용자, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,4 +1,3 @@
-import copy
 def solution(user_id, banned_id):
     # DFS??
     # 불량 사용자 가능 조건
@@ -7,7 +6,7 @@ def solution(user_id, banned_id):
     # banned_id 1차원 반복, user_id에서 적절한값 찾으면 다음 분기로
     # 종료조건 : depth == banned_id의 길이
     
-    ans = set()
+    ans = 0
     lengths = len(user_id)
     visited = [0] * lengths
     
@@ -26,21 +25,18 @@ def solution(user_id, banned_id):
         if count == len(banId) : return True
         return False
                 
-    def DFS (depth,arr) :
-        if depth == len(banned_id) :
-            newArr = copy.copy(arr)
-            newArr.sort()
-            ans.add(''.join(newArr))
-            return
+    def DFS (depth) :
+        global ans
         
+        if depth == len(banned_id) :
+            print('yes') 
+            return
         for i in range(lengths):
             if visited[i] : continue
             if check(banned_id[depth],user_id[i]) :
                 visited[i] = 1
-                arr.append(user_id[i])
-                DFS(depth+1,arr)
+                DFS(depth+1)
                 visited[i] = 0
-                arr.pop()
-    DFS(0,[])
-    return(len(ans))
-     
+    DFS(0)
+    return ans
+                
