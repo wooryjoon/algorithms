7f3c210ab243be2bba94a5c2d19347a624f9323b
heejoon park <wooryjoon@gmail.com>
2023-06-20 13:27:42 +0900

    [level 3] Title: 불량 사용자, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,3 +1,4 @@
+import copy
 def solution(user_id, banned_id):
     # DFS??
     # 불량 사용자 가능 조건
@@ -5,16 +6,16 @@ def solution(user_id, banned_id):
         # 별표를 제외한 문자 동일
     # banned_id 1차원 반복, user_id에서 적절한값 찾으면 다음 분기로
     # 종료조건 : depth == banned_id의 길이
-    
-    ans = 0
+
+    ans = set()
     lengths = len(user_id)
     visited = [0] * lengths
-    
+
     def check(banId,userId) :
         ban_length = len(banId)
         user_length = len(userId)
         count = 0
-        
+
         if ban_length != user_length : return False
         for i in range(user_length):
             if banId[i] == '*' :
@@ -24,19 +25,21 @@ def solution(user_id, banned_id):
                 count += 1
         if count == len(banId) : return True
         return False
-                
-    def DFS (depth) :
-        global ans
-        
+
+    def DFS (depth,arr) :
         if depth == len(banned_id) :
-            print('yes') 
+            newArr = copy.copy(arr)
+            newArr.sort()
+            ans.add(''.join(newArr))
             return
+
         for i in range(lengths):
             if visited[i] : continue
             if check(banned_id[depth],user_id[i]) :
                 visited[i] = 1
-                DFS(depth+1)
+                arr.append(user_id[i])
+                DFS(depth+1,arr)
                 visited[i] = 0
-    DFS(0)
-    return ans
-                
+                arr.pop()
+    DFS(0,[])
+    return(len(ans))
