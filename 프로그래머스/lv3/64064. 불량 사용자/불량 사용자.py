d1b2467c50db12db1caf2467373b4cd6d5d834de
heejoon park <wooryjoon@gmail.com>
2023-06-20 10:46:16 +0900

    [level 3] Title: 불량 사용자, Time: 0.10 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,46 @@
+import copy
+def solution(user_id, banned_id):
+    # DFS??
+    # 불량 사용자 가능 조건
+        # 문자열 길이 동일
+        # 별표를 제외한 문자 동일
+    # banned_id 1차원 반복, user_id에서 적절한값 찾으면 다음 분기로
+    # 종료조건 : depth == banned_id의 길이
+    
+    ans = set()
+    lengths = len(user_id)
+    visited = [0] * lengths
+    
+    def check(banId,userId) :
+        ban_length = len(banId)
+        user_length = len(userId)
+        count = 0
+        
+        if ban_length != user_length : return False
+        for i in range(user_length):
+            if banId[i] == '*' :
+                count += 1
+                continue
+            if banId[i] == userId[i] :
+                count += 1
+        if count == len(banId) : return True
+        return False
+                
+    def DFS (depth,arr) :
+        if depth == len(banned_id) :
+            newArr = copy.copy(arr)
+            newArr.sort()
+            ans.add(''.join(newArr))
+            return
+        
+        for i in range(lengths):
+            if visited[i] : continue
+            if check(banned_id[depth],user_id[i]) :
+                visited[i] = 1
+                arr.append(user_id[i])
+                DFS(depth+1,arr)
+                visited[i] = 0
+                arr.pop()
+    DFS(0,[])
+    return(len(ans))
+     
