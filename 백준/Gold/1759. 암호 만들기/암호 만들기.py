342704193e2b12c5938a282468715b04d799cde9
heejoon park <wooryjoon@gmail.com>
2023-09-03 18:54:17 +0900

    [Gold V] Title: 암호 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,41 +1,34 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-#[문제 정리]
-# 암호 : 최소 모음1개, 최소 자음 2개 총 서로다른 L개
-# 암호는 오름차순 추측 ( == 가능성 있는 암호)
-# C개의 문자 후보를 통해서 가능성 있는 암호를 모두 구하라.
+# 암호 : L개의 알파벳으로 구성
+# 최소 조건 : 모음 1개 자음 2개
+# 알파벳이 증가하는 순서로 배열
+# C개의 알파벳을 조합해 길이가 L인 암호를 만든다.
 
-# [아이디어]
-# 배열을 오름차순으로 정렬
-# L개에서 멈추고 출력한다. DFS백트래킹
-# 그런데, 배열은 최소 하나의 모음이 있어야 한다.
-# [시간복잡도]
-
-# [변수 사용계획]
-# ??
-
-L,C = list(map(int,input().split()))
-alphabets = list(input().rstrip().split())
+L,C = list(map(int,input().split())) 
+alphabets = list(input().strip().split())
 alphabets.sort()
+ans = []
+vowel = set(['a','e','i','o','u'])
+# DFS로 길이가L인 문자열을 만든다.
+# 다만, 최소 조건을 만족하지 못하는 경우에는 pass
 
+def isVowel (alpha) :
+    if alpha in vowel:
+        return True
+    return False
 
-def DFS(arr,depth,startIndex) :
-    if depth == L :
-        odd = 0
-        even = 0
-        for x in arr :
-            if x == 'a' or x == 'e' or x == 'i' or x == 'u' or x == 'o' :
-                odd += 1
-            else: even += 1
-        if odd >= 1 and even >= 2 : print(''.join(map(str,arr)))    
+def DFS(startIdx,depth,arr,vowels):
+    if vowels >= L - 1 : return # 이미 모음이 L-1개가 차버려서, 자음 2개를 못 넣는 경우
+    if depth == L and vowels : 
+        ans.append(''.join(arr))
         return
-    else:
-        for i in range(startIndex,C):
-            arr.append(alphabets[i])
-            DFS(arr,depth+1,i+1)
-            arr.pop()
-DFS([],0,0)
+    for i in range(startIdx,C):
+        arr.append(alphabets[i])
+        if isVowel(alphabets[i]): DFS(i+1,depth+1,arr,vowels+1)
+        else : DFS(i+1,depth+1,arr,vowels)
+        arr.pop()
 
-
+DFS(0,0,[],0)
+print('\n'.join(ans))
