ad7812e0ab7c9656206c319e0efbfac7af83af0b
heejoon park <wooryjoon@gmail.com>
2023-09-03 18:45:36 +0900

    [Gold III] Title: 다리 만들기, Time: 276 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,61 +1,84 @@
-# 각 섬을 -1로 표시하고, 섬의 모든 칸에서 BFS로 표시하면서 나아가기
-# 나아가면서 겹치는곳은 방문 안하게 해놓고, 인접노드가 -1인곳을 만나면, 그대로 리턴
+import sys
 from collections import deque
-import copy
-n = int(input())
-board = [list(map(int,input().split())) for _ in range(n)] 
+input = sys.stdin.readline
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-answer = []
-cnt = 2
-def BFS (i,j,value) :
-    queue = deque()
-    queue.append([i,j,0])
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    visited[i][j] = True # BFS 내에서 따로 체크를 위한 복사맵
-    while (queue):
-        [x,y,count] = queue.popleft()
+# 입력 사이즈
+    # N : 100 이하의 자연수
+# 문제 정리
+    # N * N 지도에 여러개의 대륙이 존재
+    # 한 대륙과 다른 한 대륙 사이를 이을 수 있는 가장 짧은 길이의 다리 찾기
+# How To Solve?
+
+    # 1. 입력
+N = int(input())
+board = [list(map(int,input().split())) for _ in range(N)]
+    # 2. 필요 변수 선언 :ans, visited, BFS_func
+ans = 99999999
+visited = [[0] * N for _ in range(N)]
+location = [[] for _ in range(N * N + 1)]
+dx = (0,0,1,-1)
+dy = (1,-1,0,0)
+
+def BFS(ground,type) :
+    global ans
+    # BFS
+    q = deque()
+    for e in ground :
+        x,y = e
+        p = (x,y,0)
+        q.append(p)
+    while q :
+        x,y,length = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 범위벗어나거나, 이미방문한칸이거나, 같은섬이면 패스
-            if (nx < 0 or ny < 0 or nx >= n or ny >= n or visited[nx][ny] == True or map[nx][ny] == value):
-                continue
-            if (map[nx][ny] == 0) : 
-                # 인접한 칸이 바다일 때
-                queue.append([nx,ny,count + 1])
-                visited[nx][ny] = True
-            elif (map[nx][ny] != 0 and map[nx][ny] != value): # 인접한 칸이 바다도 아니고, 자기랑 연결된 자기섬도 아닐때 즉, 다른섬
-                answer.append(count)
-                return
-
-# 섬에 id부여하는 함수
-def BFS2 (i,j,cnt) :
-    queue = deque()
-    queue.append([i,j])
-    map[i][j] = cnt
-    while(queue):
-        [x,y] = queue.popleft()
+            nx,ny = x + dx[i], y + dy[i]
+            # 범위 체크
+            if 0<=nx<N and 0<=ny<N:
+                # 주변 노드가 방문한 땅이면 x
+                if visited[nx][ny] == 1: continue
+                # 주변 노드가 같은 땅이면 x
+                if board[nx][ny] == type : continue
+                # 주변 노드가 빈칸이면 다리 길이 + 1, 방문 처리 하고 큐에 넣기
+                if board[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny,length + 1))
+                # 주변 노드가 다른 땅이면? -> 정답 갱신 후 바로 리턴
+                if board[nx][ny] > 0 :
+                    ans = min(ans,length)
+                    return
+def grouping(x,y,team) :
+    q = deque()
+    visited[x][y] = 1
+    q.append((x,y))
+    while q :
+        x,y = q.popleft()
+        board[x][y] = team
+        location[team].append((x,y))
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (nx >= 0 and ny >= 0 and nx < n and ny < n and map[nx][ny] == 1):
-                queue.append([nx,ny])
-                map[nx][ny] = cnt
+            nx,ny = x + dx[i], y + dy[i]
+            if 0<=nx<N and 0<=ny<N :
+                if visited[nx][ny] == 1 : continue
+                if board[nx][ny] == 1:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
 
-# 각 떨어진 섬들마다 id 부여
-map = copy.deepcopy(board)
-for i in range(n) : 
-    for j in range(n):
-        if (map[i][j] == 1) : 
-            BFS2(i,j,cnt)
-            cnt = cnt + 1
 
-# 중첩 반복을 통해 최단거리 찾기
-for i in range(n):
-    for j in range(n):
-        if (board[i][j] > 0):
-            BFS(i,j,map[i][j])
-print(min(answer))
+# 3. 지도를 2중 For문으로 순회하며, 대륙마다 그룹핑
+team = 0
+for i in range(N) :
+    for j in range(N):
+        if visited[i][j] == 1 : continue
+        if board[i][j] == 0 : continue
+        team += 1
+        grouping(i,j,team)
+# 4. 각 그룹마다 한번씩 자기 땅의 모든 칸에서 BFS 출발
+for i in range(1,team + 1) :
+    visited = [[0] * N for _ in range(N)]
+    BFS(location[i],i)
+# 5. ans 변수 갱신 후 정답 출력
+print(ans)
+
+
+
+
+
 
